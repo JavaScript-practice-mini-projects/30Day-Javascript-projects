078553dbc6643a6fadcbe7b0defb72b02fd5a86c
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2024-12-19 13:02:06 +0600

    quick commit

--- Weather-App/index.js
+++ Weather-App/index.js
@@ -5,7 +5,28 @@ const searchBox = document.querySelector('.search input')
 const searchButton = document.querySelector('.search button')
 const weatherIcon = document.querySelector('.weather-icon')
 const errorMessage = document.querySelector('.error')
+const temperature = document.querySelector('.temp')
+const cityName = document.querySelector('.city')
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
 
+const weatherIcons = {
+    rain: './img/rain.png',
+    clear: './img/clear-sun.png',
+    cloud: './img/cloudy.png',
+    drizzle: './img/drizzle.png',
+    mist: './img/mist.png',
+    snow: './img/snow.png',
+    haze: './img/haze.png',
+    'clear sky': './img/clear-sky.png'
+}
+
+weatherCheck()
+
+searchButton.addEventListener('click', () => {
+    city = searchBox.value
+    weatherCheck()
+})
 
 
 async function weatherCheck() {
@@ -13,57 +34,31 @@ async function weatherCheck() {
     const apiKey = '4141d67a67254dd7fac44aa883b116a8';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
     try{
-        const response = await fetch(apiUrl)
+    const response = await fetch(apiUrl)
 
-        if(response.status == 404){
-            errorMessage.style.opacity = '1'
-        }else{
-            errorMessage.style.opacity = '0'
-        
-            if(!response.ok){
-                throw new Error(`HTTP error ${response.status}`)
-            }    
-            const data = await response.json()
-            console.log()
-            document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C'
-            document.querySelector('.city').innerHTML = data.name
-            document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
-            document.querySelector('.wind').innerHTML = data.wind.speed + 'Km/h'
-            
-            if(data.weather[0].main == 'Rain'){
-                weatherIcon.src = './img/rain.png'
-            }
-            else if(data.weather[0].main == 'Clear'){
-                weatherIcon.src = './img/clear-sun.png'
-            }
-            else if(data.weather[0].main == 'Cloudy'){
-                weatherIcon.src = './img/cloudy.png'
-            }
-            else if(data.weather[0].main == 'Drizzle'){
-                weatherIcon.src = './img/drizzle.png'
-            }
-            else if(data.weather[0].main == 'Mist'){
-                weatherIcon.src = './img/mist.png'
-            }
-            else if(data.weather[0].main == 'Snow'){
-                weatherIcon.src = './img/snow.png'
-            }
-            else if(data.weather[0].main == 'Haze'){
-                weatherIcon.src = './img/haze.png'
-            }
-            
+    if(response.status == 404){
+        errorMessage.style.opacity = '1'
+    }else{
+        errorMessage.style.opacity = '0'
+    
+        if(!response.ok){
+            throw new Error(`HTTP error ${response.status}`)
+        }    
+        const data = await response.json()
+        updateWeatherUi(data)
+    }
+    }catch(error){
+        console.error(error.message)
         }
-        }catch(error){
-            console.error(error.message)
-        }
-        
-
 }
 
-weatherCheck()
+function updateWeatherUi(data){
+    temperature.innerHTML = Math.round(data.main.temp) + '°C'
+    cityName.innerHTML = data.name
+    humidity.innerHTML = data.main.humidity + '%'
+    wind.innerHTML = data.wind.speed + 'Km/h'
+    
+    const weatherType = data.weather[0].description
+    weatherIcon.src = weatherIcons[weatherType]
+}
 
-searchButton.addEventListener('click', () => {
-    city = ''
-    city = searchBox.value
-    weatherCheck()
-})
